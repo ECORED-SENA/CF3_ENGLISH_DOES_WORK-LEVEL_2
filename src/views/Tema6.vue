245257642e570efaf3e9bd0099b69a3dab9d2cf4
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-07-08 13:22:14 -0500

    Tema 6

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,18 +5,187 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero.text-white
         span 6
-      h1.fst-italic 
-    p.mb-5(data-aos='fade-right') Las preposiciones de tiempo (en inglés, “prepositions of time”) son expresiones que nos dan información sobre el tiempo en que sucede algo y se emplean para hablar del momento en que se realiza una acción.
-    .tarjeta.color-acento-contenido.bg-claro.rounded-0.pt-4.px-3
-      .row.justify-content-center.align-items-center
-        .col-lg-4.col-8.mb-lg-0.mb-3
-          img(src='@/assets/curso/temas/75.png', alt='')
-        .col-lg-8
-          p.p-5.mb-0 Comúnmente, usamos las preposiciones, en general (de tiempo, de lugar, de modo, etc.), para mostrar una relación en el espacio o el tiempo, o una relación lógica entre dos o más personas, lugares o cosas.
-            br
-            br
-            |Desafortunadamente, las preposiciones pueden ser un área de la gramática compleja de dominar y no existe una fórmula mágica para ellas, ya que no nos podemos confiar al cien por ciento de su traducción. Esto se debe a que su traducción puede variar de un idioma a otro, varias preposiciones pueden traducir lo mismo al español, pero emplearse de manera diferente en inglés, por ende, <b>no se debe partir de la pregunta “¿cómo se diría esto en español?” En su lugar, cada preposición debe aprenderse basándose en su uso específico.</b>
-   
+      h1 Pasatiempos y <em>hobbies</em>
+    p.mb-5(data-aos='fade-right') Cuando se habla de pasatiempos y hobbies en inglés, se debe tener en cuenta no solo el vocabulario, sino la forma en que se presentan dichas actividades. En inglés, no se muestra una actividad con el verbo TO BE, a menos que este verbo se encuentre en forma ING.
+      br
+      br
+      |Ahora, revisemos el vocabulario para que memorice y practique:
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-3")
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/169.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic  Watch TV
+          p.mb-0 Ver televisión  
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/170.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Go out with friends 
+          p.mb-0 Salir con amigos
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/171.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Play video games
+          p.mb-0 Jugar videojuegos
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/172.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Play tennis
+          p.mb-0 Jugar tenis
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/173.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Play soccer / football
+          p.mb-0 Jugar fútbol
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/174.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Read 
+          p.mb-0 Leer
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/175.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Listen to music
+          p.mb-0 Escuchar musica
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/176.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Surf the internet
+
+          p.mb-0 Navegar en internet
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/177.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Go shopping
+          p.mb-0 Ir de compras
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/178.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Go to the cinema 
+          p.mb-0 Ir al cine 
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/179.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Do yoga 
+
+          p.mb-0 Hacer yoga
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/180.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Cycling
+          p.mb-0 Hacer ciclismo
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/181.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Hiking
+          p.mb-0 Hacer senderismo
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/182.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Exercise
+          p.mb-0 Hacer ejercicio
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/183.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Cook 
+          p.mb-0 Cocinar
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/184.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Dance
+          p.mb-0 Bailar
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/185.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic  Take photos
+          p.mb-0 Tomar fotos
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/186.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Draw 
+          p.mb-0 Dibujar
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/187.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic  Text 
+          p.mb-0 Enviar mensajes de texto
+      .tarjeta.no-repeat.d-flex.align-items-end.justify-content-center(:style="{'background-image': `url(${require('@/assets/curso/temas/188.jpg')})`}" style='height: 394px').p-4
+        .tarjeta.bg-white.mw.p-4.text-center
+          h4
+            span.r-3.fst-italic Sing 
+          p.mb-0 Cantar
+    .tarjeta.p-4.d-flex.align-items-center(style='background-color: #ffdfe0').mb-4
+      Audio.color-acento-contenido.mx-3(:audio="require('@/assets/componentes/audios/audio-ej.mp3')").me-4
+      p.mb-0 Ahora escucha, repite y revisa de nuevo el vocabulario: 
+    h5 ¿Cómo usar las actividades según la expresión?
+    p.mb-5(data-aos='fade-right')  Ahora, cada una de las actividades puede usarse de acuerdo con la expresión. Revisemos: 
+    TabsB.color-secundario.mb-5
+      .row.p-3(titulo="Case 1" :icono="require('@/assets/curso/temas/192.svg')" style='background-color= #e1e8fe !important') 
+        .col-lg-3.col-8.mb-lg-0.mb-3: img.mb-5(src='@/assets/curso/temas/189.svg', alt='')
+        .col-lg-9
+          .tarjeta.bg-white.p-4
+            h5 Case 1
+            p En caso de ser presentados como verbos principales de la oración, se conjugan según el tiempo.
+              br
+              br
+              span.fst-italic.fw-bold For example: 
+            .row.justify-content-center
+              .col-lg-6.mb-lg-0.mb-3
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="I do yoga with my friends. "
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p Yo hago yoga con mis amigos.
+              .col-lg-6
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="We take photos on our trips. "
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p Nosotros tomamos fotos en nuestros viajes.
+      .row.p-3(titulo="Case 2" :icono="require('@/assets/curso/temas/193.svg')" style='background-color= #e1e8fe !important') 
+        .col-lg-3.col-8.mb-lg-0.mb-3: img.mb-5(src='@/assets/curso/temas/190.svg', alt='')
+        .col-lg-9
+          .tarjeta.bg-white.p-4
+            h5 Case 2
+            p En caso de ser presentado como un hobby, después del verbo TO BE, se debe mostrar con la terminación -ing.
+              br
+              br
+              span.fst-italic.fw-bold For example: 
+            .row.justify-content-center
+              .col-lg-6.mb-lg-0.mb-3
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="My favorite hobby is doing yoga."
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p Mi pasatiempo favorito es hacer yoga.
+              .col-lg-6
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="My favorite activities are singing, taking photos and texting."
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p Mis actividades favoritas son cantar, tomar fotos y enviar mensajes de texto.
+      .row.p-3(titulo="Case 3" :icono="require('@/assets/curso/temas/194.svg')" style='background-color= #e1e8fe !important') 
+        .col-lg-3.col-8.mb-lg-0.mb-3: img.mb-5(src='@/assets/curso/temas/191.svg', alt='')
+        .col-lg-9
+          .tarjeta.bg-white.p-4
+            h5 Case 3
+            p En caso de mostrarlo después del verbo LIKE o LOVE, los verbos se deben usar con la preposición TO.
+              br
+              br
+              span.fst-italic.fw-bold For example:
+            .row.justify-content-center
+              .col-lg-6.mb-lg-0.mb-3
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="I like to play video games."
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p  Me gusta jugar videojuegos.
+              .col-lg-6
+                TarjetaAudio.color-acento-contenido.bg-white.mb-3(
+                texto="We love to sing."
+                :audio="require('@/assets/componentes/audios/audio-ej.mp3')"
+                )
+                p Nos encanta cantar.
+
 
 
 
@@ -69,4 +238,20 @@ export default {
       h6
         color: #fff
       display: block
+.no-repeat
+  background-size: cover
+  background-position: center
+  background-repeat: no-repeat
+  height: 100%
+  width: 100%
+  position: relative
+.slyder-f__btn
+  .fa-chevron-right:before
+    content: url('../assets/curso/temas/iz.svg') !important
+  .fa-chevron-left:before
+    content: url('../assets/curso/temas/dr.svg') !important
+.tabs-b__tab--active
+  background-color: #e1e8fe
+.tabs-b__content-item
+  background-color: #e1e8fe
 </style>
